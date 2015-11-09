cd5d6ce14248ef078a5b52ab61e793eedefba042
Jiao Lin <linjiao@ornl.gov>
2015-11-09 09:20:55 -0500

    trying to use cmake for building by pulling from experiences on numpyext

--- lib/wrap_ptr.h
+++ lib/wrap_ptr.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "boost/python/object.hpp"
+#ifdef USE_PYRE
 #include "journal/debug.h"
+#endif
 
 //! wrap a pointer to become a boost python object
 /// This is not a copy!
@@ -13,10 +15,15 @@ PyObject *wrap_ptr( void *ptr )
   T  * t_ptr = static_cast<T *> (ptr);
 
 #ifdef DEBUG
+#ifdef USE_PYRE
   journal::debug_t debug("bpext.wrap_ptr");
-  debug << journal::at(__HERE__)
-	<< "pointer to c++ object: " << t_ptr 
-	<< journal::endl;
+  debug 
+    << journal::at(__HERE__)
+    << "pointer to c++ object: " << t_ptr 
+    << journal::endl;
+#else
+  std::cerr << "pointer to c++ object: " << t_ptr  << std::endl;
+#endif
 #endif
 
   //std::cout << t_ptr << std::endl;
@@ -24,11 +31,12 @@ PyObject *wrap_ptr( void *ptr )
     return incref(converter::detail::pointer_shallow_arg_to_python<T *>(t_ptr).get());
   }
   catch (...) {
-    std::cerr << "Cannot convert the given pointer to a boost python object!!!\n"
-              << "Will return a NULL pointer from " << __FILE__ 
-              << " line " << __LINE__ 
-              << " in function wrap_ptr."
-              << std::endl;
+    std::cerr 
+      << "Cannot convert the given pointer to a boost python object!!!\n"
+      << "Will return a NULL pointer from " << __FILE__ 
+      << " line " << __LINE__ 
+      << " in function wrap_ptr."
+      << std::endl;
     return NULL;
   }
 }
